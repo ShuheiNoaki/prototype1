fc18c0e17bdce9339867d045928b0f3a25dd10b6
shuheinoaki <shunnnk1015@gmail.com>
2017-06-18 14:14:42 +0900

    modify controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,24 @@
 class MessagesController < ApplicationController
+	before_action 
+
+	def create
+		@message = Message.new(message_params)
+	end
+
+	def new
+	end
+
+	def edit
+	end
+
+	def show
+		@messages = Message.where("farm_id = ? and send_id = ?", 1, params[:id])
+	end
+
+	private
+
+	def message_params
+		params.require(:message).permit(:content, :farm_id, :send_id)
+	end
+
 end
